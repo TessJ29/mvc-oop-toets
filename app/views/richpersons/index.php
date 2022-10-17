13fdc8e44053b67692ca025090c2ed7903f3312d
Tess Jansen <tess.jansen29@gmail.com>
2022-10-17 11:16:47 +0200

    Added view for richpersons

---
+++ app/views/richpersons/index.php
@@ -0,0 +1,21 @@
+<?php echo $data["title"];
+
+?>
+<table>
+    <thead>
+        <th>id</th>
+        <th>Land</th>
+        <th>capital</th>
+        <th>Continent</th>
+        <th>Population</th>
+        <th>Update</th>
+        <th>Delete</th>
+    </thead>
+    <tbody>
+        <?= $data['persons'] ?>
+    </tbody>
+</table>
+
+<button>
+    <a href="<?= URLROOT; ?>/homepages/index">Terug</a>
+</button>
